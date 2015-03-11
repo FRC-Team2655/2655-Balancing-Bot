3f9e06fa10da5d0e006a7b4dde9be9d223e82de2
magicknees <magicknees@gmail.com>
2015-03-10 22:18:08 -0400

    add various local libraries to the project. starting to work through compiler issues to get accelerometer working.

--- JohnBot/JohnBot.ino
+++ JohnBot/JohnBot.ino
@@ -1,9 +1,16 @@
+#include <ck1122.h>
+
+
+#include <I2Cdev.h>
+
+#include <Wire.h>
+
 #include <helper_3dmath.h>
 #include <MPU6050.h>
-#include <MPU6050_6Axis_MotionApps20.h>
-#include <MPU6050_9Axis_MotionApps41.h>
 
-#include "ck1122.h"
+//#include <MPU6050_6Axis_MotionApps20.h>
+//#include <MPU6050_9Axis_MotionApps41.h>
+
 
 
 
@@ -15,29 +22,40 @@ const int groundpin = 52;             // analog input pin 4 -- ground
 const int powerpin = 53;              // analog input pin 5 -- voltage
 
 ck1122 mc(1,2,3,4,5,6);
-MPU6050 gyro();
+
+MPU6050 *gyro;
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
+
 
 void setup()
 {
   Serial.begin(9600); 
 
+/*
   pinMode(groundpin, OUTPUT);
   pinMode(powerpin, OUTPUT);
   
   digitalWrite(groundpin, LOW);
   digitalWrite(powerpin, HIGH);
-  
-  gyro.initialize();
+*/
+  gyro = new MPU6050();
+  gyro->initialize();
 }
 
 void loop()
 {
-    gyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    gyro->getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
     
-    mc.setSpeed(1,2);
+    Serial.print("ax="); Serial.print(ax);
+    Serial.print("   ay="); Serial.print(ay);
+    Serial.print("   az="); Serial.print(az);
+    Serial.print("   gx="); Serial.print(gx);
+    Serial.print("   gy="); Serial.print(gy);
+    Serial.print("   gz="); Serial.print(gz);
+    Serial.print("\n");    
+    //mc.setSpeed(1,2);
 
 }
 
