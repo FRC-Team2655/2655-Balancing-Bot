33e9ce63575d3eb25bd92ce3e93180b3ebe2c7c1
Isaac Spencer <warrior7512013@gmail.com>
2015-04-13 21:01:01 -0400

    2nd Version of Isaacs code

--- IceBot/IceBot.ino
+++ IceBot/IceBot.ino
@@ -1,25 +1,41 @@
 
-#include <ck1122.h>
-#include "I2Cdev.h"
-#include "MPU6050.h"
-
-#include "Wire.h"
-
-MPU6050 *accelgyro;
-
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-
-
+int i;
+char t;
 void setup()
 {
-  Serial.begin(9600); 
+  i = 9600;
+  Serial.begin(9600);
+  t = 1;
+  
+  
 }
 
+char x;
 void loop()
 {
-  static int i = 0;
-  Serial.print("hi "); Serial.print(i++); Serial.print("\n");  
+#define CR " n"
+
+  if (Serial.available() )  {
+    x = Serial.read();
+    Serial.print("x = ");
+    Serial.print(x);
+    Serial.print("\n");
+    if (x == 'r') {
+      i = 9600;
+    }
+   if (x == '1') t = 1;
+   
+   if (x == '2') t = 2;
+   }
+
+  Serial.print("hi ");
+
+  Serial.print(i);
+
+  Serial.print("\n");
+
+  i = i + t;
+
   delay(200);
 }
 
